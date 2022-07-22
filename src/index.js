c6efa38b3ad3b1cd7071f4d946789aefc4cd4b17
Anton <volkovanton71@yandex.ru>
2022-07-22 17:06:49 +0300

    fddf

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import './assets/styles/style.scss';
+// import './assets/styles/style.scss';
 console.log('Hello main.js!')
 
 import test from './test'
-
+// test()
+// console.log('te',test());
 document.body.innerHTML = test()
--- src/test.js
+++ src/test.js
@@ -1,19 +1,53 @@
-import {render} from "pug"
-import fs from 'fs'
+import {render, renderFile} from "pug"
+// import fs from 'fs'
+//
+// function test() {
+//     const template = fs.readFileSync(__dirname + './test.pug', 'utf8')
+//
+//
+//     const data = {
+//         values: [
+//             {id: 1, description: "dsd"},
+//             {id: 2, description: "dsd"}
+//         ]
+//     };
+//     const output = render(template, data);
+//
+//     return output
+// }
+//
+// export default test
 
-function test() {
-    const template = fs.readFileSync(__dirname + './test.pug', 'utf8')
+// import html from 'bundle-text:./templates/test.pug';
+//
+// function test(){
+//     // const tt = render(html, {test2: 'ddddddddd'})
+//     // console.log('tt',tt);
+//     return html
+// }
+//
+// export default test
 
+// function test(){
+//     const html = renderFile('./templates/test.pug', {test2: 'fffffff'})
+//
+//     console.log('html',html);
+//
+//     return html
+//
+// }
+//
+// export default test
+import fs from 'fs';
 
-    const data = {
-        values: [
-            {id: 1, description: "dsd"},
-            {id: 2, description: "dsd"}
-        ]
-    };
-    const output = render(template, data);
+const sourceCode = fs.readFileSync(__dirname + './templates/test.pug', 'utf8');
+function test(){
+    const html = render(sourceCode, {test2: 'fffffff'})
 
-    return output
+    console.log('html',sourceCode);
+
+    return html
+ br
 }
 
 export default test
