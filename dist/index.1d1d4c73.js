27db5155913fa47bb0e057e74f03a99a50fbe916
Anton <volkovanton71@yandex.ru>
2022-07-22 16:23:24 +0300

    fddf

--- dist/index.1d1d4c73.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello main.js!");
-//# sourceMappingURL=index.1d1d4c73.js.map
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-// import '../assets/styles/style.scss';
+import './assets/styles/style.scss';
 console.log('Hello main.js!')
-//
-// import test from '../test'
-//
-// document.body.innerHTML = test()
+
+import test from './test'
+
+document.body.innerHTML = test()
