6e401fcd941b1553faab96731d1bdd3385d3caf0
Anton <volkovanton71@yandex.ru>
2022-07-22 19:49:08 +0300

    simple example

---
+++ src/handlebars/index.js
@@ -0,0 +1,13 @@
+// import * as Handlebars from "handlebars";
+// import login from './test.hbs'
+console.log('sdfsjdkfj',);
+// const template = Handlebars.compile(login)
+// var template = Handlebars.template(templateSpec);
+// const context = {firstname: 'name'}
+// const html = login(context)
+// console.log('template',html);
+
+// const $root = document.querySelector('#root')
+// console.log('html',html); //http://localhost:1234/test.5882c462.html?1658506458646
+// $root.innerHTML =  login(context)
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
-// import './assets/styles/style.scss';
-console.log('Hello main.js!')
+// // import './assets/styles/style.scss';
+// console.log('Hello main.js!')
+//
+// import test from './test'
+// // test()
+// // console.log('te',test());
+// document.body.innerHTML = test()
 
-import test from './test'
-// test()
-// console.log('te',test());
-document.body.innerHTML = test()
+import tpl from './index.hbs';
+
+const data = {
+    h1: 'Title',
+    content: 'Содержимое'
+}
+
+console.log(tpl(data));
+
+import './style.css';
+import button from './components/button';
+import page1 from './pages/page1';
+import page2 from './pages/page2';
+
+console.log(tpl);
+
+document.getElementById('root').innerHTML = tpl({
+    fname: 'students',
+    btn: button('btn1','Click this')
+});
+
+
+window.createButton = (id, value) => {
+
+    const htmlTpl = document.createElement('template');
+    htmlTpl.innerHTML = button(id,value);
+
+    document.getElementById('root').appendChild(htmlTpl.content);
+}
